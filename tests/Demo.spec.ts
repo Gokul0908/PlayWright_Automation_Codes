d24488235c257c38857119b8e1eaaac614026ab2
gokul.selvarasu <gokulselvarasu0908@gmail.com>
2026-05-23 14:33:04 +0530

    Playwright

---
+++ tests/Demo.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+
+test("Verifying the page title", async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    let title = await page.title();
+    console.log("The Title is: " + title);
+    await expect(page).toHaveTitle(/Automation Testing Practice/);
+});
+
+test("Verifying the page url", async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    let url = await page.url();
+    console.log("The URL is: " + url);
+    await expect(page).toHaveURL(/testautomationpractice\.blogspot\.com/);
+});
