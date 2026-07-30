06ff0e93d0a2c4da65eeafb3fdfb3b3cf48c2794
gokul.selvarasu <gokulselvarasu0908@gmail.com>
2026-07-30 14:55:32 +0530

    Extra spaces removed

--- tests/Pratices/pr_windowhandle.spec.ts
+++ tests/Pratices/pr_windowhandle.spec.ts
@@ -1,36 +1,33 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test, expect, chromium } from "@playwright/test";
 
 test("Window Handle - Extract All Pages", async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const parentPage = await context.newPage();
 
-    const browser = await chromium.launch();
-    const context = await browser.newContext();
-    const parentPage = await context.newPage();
+  await parentPage.goto("https://testautomationpractice.blogspot.com/");
 
-    await parentPage.goto('https://testautomationpractice.blogspot.com/');
+  const [newPage] = await Promise.all([
+    parentPage.waitForEvent("popup"),
+    parentPage.click("#PopUp"),
+  ]);
 
-    const [newPage] = await Promise.all([
-        parentPage.waitForEvent('popup'),
-        parentPage.click('#PopUp'),
-    ]);
+  await newPage.waitForLoadState("load");
 
-    await newPage.waitForLoadState('load');
+  const allPages = parentPage.context().pages();
+  console.log(`Total pages: ${allPages.length}`);
 
-    const allPages = parentPage.context().pages();
-    console.log(`Total pages: ${allPages.length}`);
+  for (const p of allPages) {
+    const URL = p.url();
+    const TITLE = await p.title();
 
-    for (const p of allPages) {
+    // console.log(`The page if url is==>${URL}`);
+    // console.log
+    // console.log("The Current Page URL is ===> : " + URL);
+    // console.log("The Current Page Title is==> : " + TITLE);
+  }
+  const secondPopup = allPages[2];
+  // await secondPopup.waitForLoadState('domcontentloaded');
 
-        const URL = p.url();
-        const TITLE = await p.title();
-
-        // console.log(`The page if url is==>${URL}`);
-        // console.log
-        // console.log("The Current Page URL is ===> : " + URL);
-        // console.log("The Current Page Title is==> : " + TITLE);
-    }
-
-    const secondPopup = allPages[2];
-    // await secondPopup.waitForLoadState('domcontentloaded');
-
-    await secondPopup.locator('.getStarted_Sjon').click();
+  await secondPopup.locator(".getStarted_Sjon").click();
 });
